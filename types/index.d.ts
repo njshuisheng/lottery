70c91ef7001da5971c433f9d68b372a1678afe62
hehongsheng <hehongsheng@dolinkhub.com>
2022-07-01 18:17:55 +0800

    chore:publish

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,3 @@
-/// <reference path="./types/red.d.ts" />
-export type * from './red.d.ts'
 export type Red = '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17' | '18' | '19' | '20' | '21' | '22' | '23' | '24' | '25' | '26' | '27' | '28' | '29' | '30' | '31' | '32' | '33'
 export type Blue = '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10' | '11' | '12' | '13' | '14' | '15' | '16'
 export interface Lottery {
@@ -8,3 +6,4 @@ export interface Lottery {
     reds: string
     blue: string
 }
+export * from '../src'
